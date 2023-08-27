ec3bcc2b94a48397f63aa74971238c671187ae05
TK17250 <TXK285@gmail.com>
2023-08-27 16:57:54 +0700

    add grahp date in Homepage

--- component/Home/Home.jsx
+++ component/Home/Home.jsx
@@ -4,9 +4,36 @@ import { StyleSheet } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { AntDesign } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { BarChart } from "react-native-gifted-charts";
 
 export default function Home({ boxstatus, statusHomepage }) {
     const [modalVisible, setModalVisible] = React.useState(false);
+
+    const data = [
+        { value: 30, label: '27', frontColor: '#368F8B' },
+        { value: 15, label: '28', frontColor: '#FF0000' },
+        { value: 23, label: '29', frontColor: '#FFFF00' },
+        { value: 9, label: '30', frontColor: '#FF0000' },
+        { value: 50, label: '31', frontColor: '#368F8B' },
+    ]
+
+    const colordata = () => {
+        return data.map(item => {
+            if (item.value > 29) {
+                item.frontColor = "#368F8B";
+            } else if (item.value > 19) {
+                item.frontColor = "#FFFF00";
+            } else {
+                item.frontColor = "#FF0000";
+            }
+            return item;
+        });
+    }
+
+    React.useEffect(() => {
+        colordata();
+    }, [data]);
+
     return (
         <View style={styles.container}>
             {/*     Box Status   */}
@@ -19,6 +46,21 @@ export default function Home({ boxstatus, statusHomepage }) {
             <View style={{ marginTop: 20, }}>
                 <Text style={styles.status}>{statusHomepage}</Text>
             </View>
+            {/*     Graph Date   */}
+            <View style={{ marginTop: 20, }}>
+                <BarChart
+                    showFractionalValue
+                    showYAxisIndices
+                    backgroundColor="#1F1639"
+                    hideYAxisText={true}
+                    initialSpacing={25}
+                    noOfSections={4}
+                    hideRules
+                    data={data}
+                    isAnimated
+                    width={300}
+                />
+            </View>
             {/*     How to Use    */}
             <TouchableOpacity style={styles.HowToUse} onPress={() => setModalVisible(true)}>
                 <AntDesign name="questioncircleo" style={styles.HowToUse.icon} />
@@ -27,16 +69,16 @@ export default function Home({ boxstatus, statusHomepage }) {
             <View style={styles.warn}>
 
                 {/*     Water   */}
-                <View style={{width: "50%", padding: 5,}}>
-                    <Text style={{fontSize: 20, paddingBottom: 5,}}><MaterialCommunityIcons name="water-plus" size={24} color="#246A73" /> อย่าลืมดื่มน้ำ</Text>
+                <View style={{ width: "50%", padding: 5, }}>
+                    <Text style={{ fontSize: 20, paddingBottom: 5, }}><MaterialCommunityIcons name="water-plus" size={24} color="#246A73" /> อย่าลืมดื่มน้ำ</Text>
                     <Text>รู้ไหมว่าการดื่มน้ำมีส่วนช่วยในการทำงานของระบบประสาท</Text>
                 </View>
 
-                <Text style={{width: 3, height: 150, flexShrink: 0, backgroundColor: "#246A73"}}></Text>
+                <Text style={{ width: 3, height: 110, flexShrink: 0, backgroundColor: "#246A73" }}></Text>
 
                 {/*     walk     */}
-                <View style={{width: "50%", padding: 5, marginLeft: 20,}}>
-                    <Text style={{fontSize: 20, paddingBottom: 5,}}><MaterialCommunityIcons name="walk" size={24} color="#246A73" /> หมั่นเดินบ้าง</Text>
+                <View style={{ width: "50%", padding: 5, marginLeft: 20, }}>
+                    <Text style={{ fontSize: 20, paddingBottom: 5, }}><MaterialCommunityIcons name="walk" size={24} color="#246A73" /> หมั่นเดินบ้าง</Text>
                     <Text>รู้ไหมว่าการเดินมีส่วนช่วยในการทำงานของระบบประสาท</Text>
                 </View>
 
@@ -70,6 +112,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
+        width: wp('100%'),
         position: "relative",
     },
     box: {
